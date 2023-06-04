072faea7b7e231fc32e4a09231912f23aff206e8
Matt Hackmann <mhackmann13@icloud.com>
2023-06-04 15:53:24 -0500

    Add controller methods

--- app/controllers/recipe.controller.js
+++ app/controllers/recipe.controller.js
@@ -43,12 +43,48 @@ exports.findAll = (req, res) => {
 
 // Find a single Recipe with a id
 exports.findOne = (req, res) => {
-  
+  Recipe.findById(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found recipe with id ${req.params.id}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving recipe with id " + req.params.id
+        });
+      }
+    } else res.send(data);
+  });
 };
 
 // Update a Recipe identified by the id in the request
 exports.update = (req, res) => {
-  
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  console.log(req.body);
+
+  Recipe.updateById(
+    new Recipe(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found recipe with id ${req.params.recipe.id}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating recipe with id " + req.params.recipe.id
+          });
+        }
+      } else res.send(data);
+    }
+  );
 };
 
 // Delete a Recipe with the specified id in the request
