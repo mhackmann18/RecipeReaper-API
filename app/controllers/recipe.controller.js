c5a6ef59af113ca8d467df1956a434920db25f55
Matt Hackmann <mhackmann13@icloud.com>
2023-06-04 14:06:22 -0500

    Create recipe controller

---
+++ app/controllers/recipe.controller.js
@@ -0,0 +1,31 @@
+const Recipe = require("../models/tutorial.model.js");
+
+// Create and Save a new recipe
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all recipes from the database (with condition).
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single Recipe with a id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Recipe identified by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Recipe with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all recipes from the database.
+exports.deleteAll = (req, res) => {
+  
+};
