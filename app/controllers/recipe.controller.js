dc489e756398cb9a4cac2fe78b6427507534399a
Matt Hackmann <mhackmann13@icloud.com>
2023-06-05 11:01:40 -0500

    Add controller functions for delete and deletAll

--- app/controllers/recipe.controller.js
+++ app/controllers/recipe.controller.js
@@ -92,7 +92,30 @@ exports.update = (req, res) => {
 };
 
 // Delete a Recipe with the specified id in the request
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+  Recipe.remove(req.params.id, (err /* data */) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found recipe with id ${req.params.id}.`,
+        });
+      } else {
+        res.status(500).send({
+          message: `Could not delete recipe with id ${req.params.id}`,
+        });
+      }
+    } else res.send({ message: `Recipe was deleted successfully!` });
+  });
+};
 
 // Delete all recipes from the database.
-exports.deleteAll = (req, res) => {};
+exports.deleteAll = (req, res) => {
+  Recipe.removeAll((err /* data */) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all recipes.",
+      });
+    else res.send({ message: `All recipes were deleted successfully!` });
+  });
+};
