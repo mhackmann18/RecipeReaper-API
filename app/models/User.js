41e6e8cb9c33a64ea34ab2e5bb67d5e9de39daac
Matt Hackmann <mhackmann13@icloud.com>
2023-06-28 18:22:55 -0500

    add findAll method in User model

--- app/models/User.js
+++ app/models/User.js
@@ -15,6 +15,21 @@ class User {
       conn.end();
     }
   }
+
+  static async findAll() {
+    const conn = await connectToDB();
+
+    const query = "SELECT * FROM users";
+
+    try {
+      const res = await conn.execute(query);
+      return { users: res[0] };
+    } catch (error) {
+      return { error };
+    } finally {
+      conn.end();
+    }
+  }
 }
 
 module.exports = User;
