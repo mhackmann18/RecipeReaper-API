671c8e75720a9dca7217bfe1fa0b1bcab6d99778
Matt Hackmann <mhackmann13@icloud.com>
2023-06-30 14:08:17 -0500

    Add login function

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const utils = require("../utilities/utils");
 const User = require("../models/User");
+require("dotenv").config({ path: `${__dirname}/config.env` });
 
 exports.register = async (req, res) => {
   utils.printRequest(req);
@@ -44,8 +46,13 @@ exports.register = async (req, res) => {
 
   if (error) {
     utils.printErrMsg(error);
-    res.status(500).send({ message: error.message || "Some error occurred" });
+    res
+      .status(500)
+      .send({ message: error.message.message || "Some error occurred" });
   } else {
+    user.token = jwt.sign({ username }, process.env.TOKEN_KEY, {
+      expiresIn: "2h",
+    });
     utils.printSuccessMsg();
     res.send(user);
   }
@@ -61,9 +68,9 @@ exports.findOne = async (req, res) => {
     res.send(user);
   } else if (error) {
     utils.printErrMsg(error);
-    res
-      .status(500)
-      .send({ message: error.message || "An unexpected error occurred" });
+    res.status(500).send({
+      message: error.message.message || "An unexpected error occurred",
+    });
   } else {
     utils.printErrMsg({
       message: `No user found with username '${req.params.username}'`,
@@ -84,12 +91,58 @@ exports.findAll = async (req, res) => {
     res.send(users);
   } else {
     utils.printErrMsg(error);
-    res.status(500).send({ message: error || "An unexpected error occurred" });
+    res
+      .status(500)
+      .send({ message: error.message || "An unexpected error occurred" });
   }
 };
 
-exports.login = (req, res) => {
-  res.send("user logged in");
+exports.login = async (req, res) => {
+  try {
+    utils.printRequest(req);
+
+    // Validate request
+
+    if (!req.body) {
+      throw new Error("Content cannot be empty");
+    }
+
+    const { username, password } = req.body;
+
+    if (!username) {
+      throw new Error("'username' property is required");
+    }
+    if (!password) {
+      throw new Error("'password' property is required");
+    }
+    for (const name of Object.keys(req.body)) {
+      if (name !== "password" && name !== "username") {
+        throw new Error(`Unknown property '${name}' provided`);
+      }
+    }
+
+    // Make sure user exists
+
+    const existingUser = await User.findOne(username);
+
+    if (!existingUser.user) {
+      throw new Error("No user exists with that username");
+    }
+
+    if (await bcrypt.compare(password, existingUser.user.password)) {
+      existingUser.user.token = jwt.sign({ username }, process.env.TOKEN_KEY, {
+        expiresIn: "2h",
+      });
+
+      utils.printSuccessMsg();
+      res.send(existingUser.user);
+    } else {
+      throw new Error("Incorrect password");
+    }
+  } catch (error) {
+    utils.printErrMsg(error);
+    res.status(500).send({ message: error.message || "Something went wrong" });
+  }
 };
 
 exports.update = (req, res) => {
@@ -106,6 +159,8 @@ exports.delete = async (req, res) => {
     res.send(data);
   } else {
     utils.printErrMsg(error);
-    res.status(500).send({ message: error || "An unexpected error occurred" });
+    res
+      .status(500)
+      .send({ message: error.message || "An unexpected error occurred" });
   }
 };
